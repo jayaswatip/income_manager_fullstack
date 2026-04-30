704ba747896bb4c6fd09f75b4b48739b6c910589
jayaswati_periyasamy <jayaswatip@gmail.com>
2026-04-30 23:13:42 +0530

    register.js have completed

---
+++ frontend/src/pages/Register.js
@@ -0,0 +1,68 @@
+import { useState } from "react";
+import API from "../services/api";
+import { useNavigate, Link } from "react-router-dom";
+
+function Register() {
+
+    const [form, setForm] = useState({
+        name: "",
+        email: "",
+        password: ""
+    });
+
+    const navigate = useNavigate();
+
+    const handleChange = (e) => {
+        setForm({ ...form, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            await API.post("/auth/register", form);
+
+            alert("Registered Successfully");
+            navigate("/");
+
+        } catch (error) {
+            alert(error.response?.data || "Error");
+        }
+    };
+
+    return (
+        <div>
+            <h2>Register</h2>
+
+            <form onSubmit={handleSubmit}>
+
+                <input
+                    name="name"
+                    placeholder="Name"
+                    onChange={handleChange}
+                />
+
+                <input
+                    name="email"
+                    placeholder="Email"
+                    onChange={handleChange}
+                />
+
+                <input
+                    name="password"
+                    type="password"
+                    placeholder="Password"
+                    onChange={handleChange}
+                />
+
+                <button type="submit">Register</button>
+            </form>
+
+            <p>
+                Already have an account? <Link to="/">Login</Link>
+            </p>
+        </div>
+    );
+}
+
+export default Register;
