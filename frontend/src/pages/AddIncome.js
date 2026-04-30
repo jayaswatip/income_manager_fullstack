19f66ac1aaaaa6efac5c1d39338a9dda29a79985
jayaswati_periyasamy <jayaswatip@gmail.com>
2026-04-30 23:15:30 +0530

    adding incomes have completed

---
+++ frontend/src/pages/AddIncome.js
@@ -0,0 +1,67 @@
+import { useState } from "react";
+import API from "../services/api";
+import { useNavigate } from "react-router-dom";
+
+function AddIncome() {
+
+    const [form, setForm] = useState({
+        title: "",
+        amount: "",
+        category: "",
+        date: ""
+    });
+
+    const navigate = useNavigate();
+
+    const handleChange = (e) => {
+        setForm({ ...form, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            await API.post("/income/add", form);
+            navigate("/dashboard");
+        } catch (error) {
+            alert("Error adding income");
+        }
+    };
+
+    return (
+        <div>
+            <h2>Add Income</h2>
+
+            <form onSubmit={handleSubmit}>
+
+                <input
+                    name="title"
+                    placeholder="Title"
+                    onChange={handleChange}
+                />
+
+                <input
+                    name="amount"
+                    placeholder="Amount"
+                    onChange={handleChange}
+                />
+
+                <input
+                    name="category"
+                    placeholder="Category"
+                    onChange={handleChange}
+                />
+
+                <input
+                    name="date"
+                    type="date"
+                    onChange={handleChange}
+                />
+
+                <button type="submit">Add</button>
+            </form>
+        </div>
+    );
+}
+
+export default AddIncome;
