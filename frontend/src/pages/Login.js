10b49f5e2938b1d480c6b356dd4d49d42a856f3a
jayaswati_periyasamy <jayaswatip@gmail.com>
2026-04-30 23:14:35 +0530

    login.js have completed

---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import API from "../services/api";
+import { useNavigate, Link } from "react-router-dom";
+
+function Login() {
+
+    const [form, setForm] = useState({
+        email: "",
+        password: ""
+    });
+
+    const navigate = useNavigate();
+
+    const handleChange = (e) => {
+        setForm({ ...form, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const res = await API.post("/auth/login", form);
+
+            localStorage.setItem("token", res.data.token);
+
+            navigate("/dashboard");
+
+        } catch (error) {
+            alert("Invalid Credentials");
+        }
+    };
+
+    return (
+        <div>
+            <h2>Login</h2>
+
+            <form onSubmit={handleSubmit}>
+                <input
+                    name="email"
+                    placeholder="Email"
+                    onChange={handleChange}
+                />
+
+                <input
+                    name="password"
+                    type="password"
+                    placeholder="Password"
+                    onChange={handleChange}
+                />
+
+                <button type="submit">Login</button>
+            </form>
+
+            <p>
+                Don't have an account? <Link to="/register">Register</Link>
+            </p>
+        </div>
+    );
+}
+
+export default Login;
